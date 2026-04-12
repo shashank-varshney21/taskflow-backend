32455aeceef2b760cad4fc22e7f535f716c98068
Shashank <varshneys495@gmail.com>
2026-04-12 20:38:54 +0530

    Add CustomUserDetailService, Update UserRepository

--- src/main/java/com/shashank/taskflow/Repositories/UserRepository.java
+++ src/main/java/com/shashank/taskflow/Repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.shashank.taskflow.Repositories;
 import com.shashank.taskflow.Entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String username);
 }
