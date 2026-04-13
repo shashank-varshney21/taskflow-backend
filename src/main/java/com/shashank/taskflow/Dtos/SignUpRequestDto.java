90a8051abc9ac5f735c51904c6a0105976fb57ba
Shashank <varshneys495@gmail.com>
2026-04-14 02:00:55 +0530

    Add application-example.properties. Add validation annotations in SignUpRequestDto

--- src/main/java/com/shashank/taskflow/Dtos/SignUpRequestDto.java
+++ src/main/java/com/shashank/taskflow/Dtos/SignUpRequestDto.java
@@ -1,5 +1,7 @@
 package com.shashank.taskflow.Dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequestDto {
+    @NotNull(message = "is required")
+    @Email(message = "Invalid email")
     private String email;
     private String name;
+    @NotNull(message = "is required")
     private String password;
 }
